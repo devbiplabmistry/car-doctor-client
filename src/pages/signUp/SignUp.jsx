9a6a23a5ba9ba10535e6512a9f934f76e6e1007b
devbiplabmistry <dev.biplabmistry@gmail.com>
2023-05-10 16:28:07 +0530

    login and sign up page

--- src/pages/signUp/SignUp.jsx
+++ src/pages/signUp/SignUp.jsx
@@ -1,9 +1,9 @@
-import loginImg from '../../assets/images/login/login.svg'
 import { AiOutlineGoogle,AiFillLinkedin } from "react-icons/ai";
 import { RiFacebookBoxFill} from "react-icons/ri";
 import { Link } from 'react-router-dom';
+import loginImg from '../../assets/images/login/login.svg'
 
-const Login = () => {
+const SignUp = () => {
     return (
         <div className="hero min-h-screen bg-white">
             <div className="hero-content flex-col lg:flex-row">
@@ -15,6 +15,12 @@ const Login = () => {
                         <form >
                             <div className="form-control">
                                 <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" name='name' placeholder="Name" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
                                 <input type="email" name='email' placeholder="email" className="input input-bordered" />
@@ -24,22 +30,19 @@ const Login = () => {
                                     <span className="label-text">Password</span>
                                 </label>
                                 <input type="text" name='password' placeholder="password" className="input input-bordered" />
-                                <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                                </label>
                             </div>
                             <div className="form-control mt-6">
-                               <input className='bg-orange-400 btn rounded-lg border-0' type="submit" value="Login" />
+                                <input className='bg-orange-400 btn rounded-lg border-0' type="submit" value="Sign Up" />
                             </div>
                         </form>
-                            <h3 className='text-center text-base mt-7 mb-7'>Or Sign In With </h3>
+                        <h3 className='text-center text-base mt-7 mb-7'>Or Sign Up With </h3>
                         <div className="another-sign flex gap-3 text-center mx-auto">
                             <AiOutlineGoogle className='w-5 h-5 rounded-[50%] bg-slate-400'></AiOutlineGoogle>
                             <AiFillLinkedin className='w-5 h-5 rounded-[50%] bg-slate-400'></AiFillLinkedin>
                             <RiFacebookBoxFill className='w-5 h-5 rounded-[50%] bg-slate-400'></RiFacebookBoxFill>
                         </div>
                         <div className='text-center mt-12'>
-                            <h4>New In  car Doctor ? <Link to="/signup" className='text-sm text-orange-400'>Sign Up</Link></h4>
+                            <h4>Already have An Account ? <Link to="/login" className='text-sm text-orange-400'>Sign In</Link></h4>
                         </div>
                     </div>
                 </div>
@@ -48,4 +51,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default SignUp;
